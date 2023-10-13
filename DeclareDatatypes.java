719ecb4f8ad19b7a41615b2e5507092bb01b31d8
Sravani7172 <sravanisravs1443@gmail.com>
2023-10-13 17:29:53 +0530

    DeclareDatatypes

---
+++ DeclareDatatypes.java
@@ -0,0 +1,15 @@
+
+public class DeclareDatatypes {
+
+    public static void main(String[] args) {
+        int integerValue=71;
+		String stringValue="Sravani";
+		double doubleValue=7.71;
+		char charValue='H';
+		System.out.println(integerValue);
+		System.out.println(stringValue);
+		System.out.println(doubleValue);
+		System.out.println(charValue);
+		
+    }
+}
