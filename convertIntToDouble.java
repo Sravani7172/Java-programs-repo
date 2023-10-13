2dc7ad09c951e4a918cf9268eb30c68c45133713
Sravani7172 <147751550+Sravani7172@users.noreply.github.com>
2023-10-13 16:51:32 +0530

    Add files via upload

---
+++ convertIntToDouble.java
@@ -0,0 +1,9 @@
+public class convertIntToDouble {
+    public static void main(String[] args) {
+        int integerValue = 10;
+		double doubleValue = 7.71;
+		double convert = (double)integerValue;
+		double result = integerValue + convert;
+		System.out.println(result);
+    }
+}
